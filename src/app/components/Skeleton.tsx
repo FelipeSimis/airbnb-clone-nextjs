9de7774e64fc263c4526e40e35f6a20990552200
Felipe Simis <felipee.oliveira17@hotmail.com>
2023-06-08 16:38:40 -0300

    Change skeleton colors and add a fullRounded prop

--- src/app/components/Skeleton.tsx
+++ src/app/components/Skeleton.tsx
@@ -1,15 +1,19 @@
 type SkeletonProps = {
   width?: string;
   height?: string;
+  fullRounded?: boolean;
 };
 
 export const Skeleton = ({
   width = 'w-full',
   height = 'h-[35vh]',
+  fullRounded,
 }: SkeletonProps) => {
   return (
     <div
-      className={`before:bg-gradient relative ${width} ${height} overflow-hidden rounded-lg bg-slate-200 before:absolute before:h-full before:w-full before:animate-skeleton before:bg-gradient-to-r before:from-transparent before:via-slate-100 before:to-transparent before:content-['']`}
+      className={`before:bg-gradient relative ${width} ${height} overflow-hidden ${
+        fullRounded ? 'rounded-full' : 'rounded-lg'
+      } bg-[#eaeaea] before:absolute before:h-full before:w-full before:animate-skeleton before:bg-gradient-to-r before:from-transparent before:via-[#f2f2f2] before:to-transparent before:content-['']`}
     />
   );
 };
