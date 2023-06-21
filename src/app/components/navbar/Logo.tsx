c1b7adb47db69fc2c186da4f696396178d06152c
Felipe Simis <felipee.oliveira17@hotmail.com>
2023-06-21 10:20:47 -0300

    Updated Logo component to adapt image priority based on viewport width

--- src/app/components/navbar/Logo.tsx
+++ src/app/components/navbar/Logo.tsx
@@ -1,16 +1,35 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
 const Logo = () => {
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <Link href="/">
+    <Link href="/" aria-label="Go to homepage">
       <Image
         src="/images/logo.png"
         width={100}
-        height={100}
+        height={31.25}
         alt="Logo"
         className="hidden cursor-pointer md:block"
-        priority
+        priority={isDesktop}
       />
     </Link>
   );
