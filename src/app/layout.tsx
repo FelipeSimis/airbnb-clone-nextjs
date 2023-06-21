b7c68cfe9c9db282f09a65a3064ce803e7c747ac
Felipe Simis <felipee.oliveira17@hotmail.com>
2023-06-21 10:10:06 -0300

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,26 @@ const RentModal = dynamic(() => import('@components/modals/RentModal'));
 export const metadata: Metadata = {
   title: 'Airbnb',
   description: 'Airbnb Clone',
+  manifest: '/favicon/site.webmanifest',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+  ],
 };
 
 const nunito = Nunito({
